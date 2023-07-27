84d7860a0fd373d730cf0bc5bad8be179ccb74a8
soondevjomer <soondevjomer@gmail.com>
2023-07-27 14:15:38 +0800

    Pinsurance first commit

---
+++ src/main/java/com/soondevjomer/pinsurance/PinsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.soondevjomer.pinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinsuranceApplication.class, args);
+	}
+
+}
